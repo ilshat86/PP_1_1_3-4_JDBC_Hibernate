9e431ba83778ba31c8e29cd0e2b5001037986b9f
Сибагатуллин Ильшат <i.sibagatullin@ant-konsalt.ru>
2022-03-17 22:37:23 +0500

    исправление замечаний по 1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -9,80 +9,106 @@ import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
     private final SessionFactory sessionFactory;
 
-    private static final String DROP_USER_SQL_TEMPLATE = "drop TABLE IF EXISTS User";
-    private static final String CREATE_USER_SQL_TEMPLATE = "create TABLE IF NOT EXISTS User (id INT auto_increment primary key, name VARCHAR(255) NOT NULL, lastName VARCHAR(255) NOT NULL, age TINYINT)";
-    private static final String DELETE_USER_SQL_TEMPLATE = "DELETE FROM User where id = :id";
-    private static final String TRUNCATE_USER_SQL_TEMPLATE = "TRUNCATE TABLE User";
-
     public UserDaoHibernateImpl() {
         this.sessionFactory = DbConnectionManager.getSessionFactory();
     }
 
-
     @Override
     public void createUsersTable() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query query = session.createSQLQuery(CREATE_USER_SQL_TEMPLATE);
-        query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        final String CREATE_USER_SQL_TEMPLATE = "create TABLE IF NOT EXISTS User (id INT auto_increment primary key, name VARCHAR(255) NOT NULL, lastName VARCHAR(255) NOT NULL, age TINYINT)";
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                Query query = session.createSQLQuery(CREATE_USER_SQL_TEMPLATE);
+                query.executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        Query query = session.createSQLQuery(DROP_USER_SQL_TEMPLATE);
-        query.executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        final String DROP_USER_SQL_TEMPLATE = "drop TABLE IF EXISTS User";
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                Query query = session.createSQLQuery(DROP_USER_SQL_TEMPLATE);
+                query.executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        User user = new User(name, lastName, age);
-        session.save(user);
-        session.getTransaction().commit();
-        //session.close();
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                User user = new User(name, lastName, age);
+                session.save(user);
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        User user = session.get(User.class, id);
-        session.remove(user);
-        session.getTransaction().commit();
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                User user = session.get(User.class, id);
+                session.remove(user);
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery cq = cb.createQuery(User.class);
-        Root<User> root = cq.from(User.class);
-        Query query = session.createQuery(cq);
-        List<User> list = query.getResultList();
-        session.getTransaction().commit();
-        return list;
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                CriteriaBuilder cb = session.getCriteriaBuilder();
+                CriteriaQuery cq = cb.createQuery(User.class);
+                Root<User> root = cq.from(User.class);
+                Query query = session.createQuery(cq);
+                List<User> list = query.getResultList();
+                session.getTransaction().commit();
+                return list;
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
+        return new ArrayList<>();
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.createSQLQuery(TRUNCATE_USER_SQL_TEMPLATE).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        final String TRUNCATE_USER_SQL_TEMPLATE = "TRUNCATE TABLE User";
+        try (Session session = sessionFactory.openSession()) {
+            try {
+                session.beginTransaction();
+                session.createSQLQuery(TRUNCATE_USER_SQL_TEMPLATE).executeUpdate();
+                session.getTransaction().commit();
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+            }
+        }
     }
 }
